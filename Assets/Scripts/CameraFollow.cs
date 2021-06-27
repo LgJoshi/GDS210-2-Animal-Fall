f15ee41f27c927a57d38987559d7aeab3c9cd95a
LgJoshi <80075442+LgJoshi@users.noreply.github.com>
2021-06-27 19:37:57 +1000

    Update CameraFollow.cs
    Made buttons appear only on game end

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     Transform animalTrans;
+    Canvas canv;
 
     // Start is called before the first frame update
     void Start()
     {
         animalTrans = GameObject.Find("Animal").GetComponent<Transform>();
+        canv=this.GetComponentInChildren<Canvas>();
+        canv.enabled=false;
     }
 
     // Update is called once per frame
@@ -17,4 +20,19 @@ public class CameraFollow : MonoBehaviour
     {
         this.transform.position = new Vector3(animalTrans.position.x, animalTrans.position.y, this.transform.position.z);
     }
+
+    void OnEnable() { 
+        EventManager.GameEnded += ToggleCanvas;
+        EventManager.GameWon += ToggleCanvas;
+    }
+    void OnDisable() {
+        EventManager.GameEnded -= ToggleCanvas;
+        EventManager.GameWon -= ToggleCanvas;
+    }
+
+
+    void ToggleCanvas(){
+        canv.enabled=true;
+    }
+
 }
